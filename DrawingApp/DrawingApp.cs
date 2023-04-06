c57dfca597fd2ff5cf521c77a1637eb12cba39be
Hazim Okanovic <62857146+HazimOkanovic@users.noreply.github.com>
2023-04-06 15:06:01 +0200

    added necessary methods and variables (#2)

--- DrawingApp/DrawingApp.cs
+++ DrawingApp/DrawingApp.cs
@@ -2,9 +2,38 @@ namespace DrawingApp
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        Pen pen;
+
+        int x = -1;
+        int y = -1;
+        bool move = false;
+
         public Form1()
         {
             InitializeComponent();
+            g = panel1.CreateGraphics();
+            pen = new Pen(Color.Black, 5);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
