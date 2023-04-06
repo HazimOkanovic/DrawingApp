a5812809db2306ba460f423035afc04938f20139
Hazim Okanovic <62857146+HazimOkanovic@users.noreply.github.com>
2023-04-06 22:13:21 +0200

    Started from scratch to develop more complex app (#4)

--- DrawingApp/DrawingApp.Designer.cs
+++ DrawingApp/DrawingApp.Designer.cs
@@ -29,109 +29,39 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            panel3 = new Panel();
             panel2 = new Panel();
-            pictureBox6 = new PictureBox();
-            pictureBox5 = new PictureBox();
-            pictureBox4 = new PictureBox();
-            pictureBox3 = new PictureBox();
-            pictureBox2 = new PictureBox();
-            pictureBox1 = new PictureBox();
             panel1.SuspendLayout();
-            panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.White;
+            panel1.Controls.Add(panel3);
             panel1.Controls.Add(panel2);
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(800, 450);
             panel1.TabIndex = 0;
-            panel1.MouseDown += panel1_MouseDown;
-            panel1.MouseMove += panel1_MouseMove;
-            panel1.MouseUp += panel1_MouseUp;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Black;
+            panel3.Dock = DockStyle.Bottom;
+            panel3.Location = new Point(0, 397);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(800, 53);
+            panel3.TabIndex = 1;
             // 
             // panel2
             // 
-            panel2.BackColor = Color.Silver;
-            panel2.Controls.Add(pictureBox6);
-            panel2.Controls.Add(pictureBox5);
-            panel2.Controls.Add(pictureBox4);
-            panel2.Controls.Add(pictureBox3);
-            panel2.Controls.Add(pictureBox2);
-            panel2.Controls.Add(pictureBox1);
-            panel2.Location = new Point(530, 12);
+            panel2.BackColor = Color.Black;
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
             panel2.Name = "panel2";
-            panel2.Size = new Size(221, 30);
+            panel2.Size = new Size(800, 100);
             panel2.TabIndex = 0;
-            // 
-            // pictureBox6
-            // 
-            pictureBox6.BackColor = Color.Maroon;
-            pictureBox6.Location = new Point(189, 3);
-            pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(20, 22);
-            pictureBox6.TabIndex = 5;
-            pictureBox6.TabStop = false;
-            pictureBox6.Click += pictureBox1_Click;
-            // 
-            // pictureBox5
-            // 
-            pictureBox5.BackColor = Color.FromArgb(128, 128, 255);
-            pictureBox5.Location = new Point(154, 3);
-            pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(20, 22);
-            pictureBox5.TabIndex = 4;
-            pictureBox5.TabStop = false;
-            pictureBox5.Click += pictureBox1_Click;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.BackColor = Color.Olive;
-            pictureBox4.Location = new Point(118, 3);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(20, 22);
-            pictureBox4.TabIndex = 3;
-            pictureBox4.TabStop = false;
-            pictureBox4.Click += pictureBox1_Click;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.BackColor = Color.Blue;
-            pictureBox3.Location = new Point(48, 3);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(20, 22);
-            pictureBox3.TabIndex = 2;
-            pictureBox3.TabStop = false;
-            pictureBox3.Click += pictureBox1_Click;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.BackColor = Color.Yellow;
-            pictureBox2.Location = new Point(83, 3);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(20, 22);
-            pictureBox2.TabIndex = 1;
-            pictureBox2.TabStop = false;
-            pictureBox2.Click += pictureBox1_Click;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BackColor = Color.Red;
-            pictureBox1.Location = new Point(12, 3);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(20, 22);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
-            pictureBox1.Click += pictureBox1_Click;
             // 
             // Paint
             // 
@@ -142,25 +72,13 @@
             Name = "Paint";
             Text = "Paint";
             panel1.ResumeLayout(false);
-            panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
+        private Panel panel3;
         private Panel panel2;
-        private PictureBox pictureBox4;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox1;
-        private PictureBox pictureBox6;
-        private PictureBox pictureBox5;
     }
 }
--- DrawingApp/DrawingApp.cs
+++ DrawingApp/DrawingApp.cs
@@ -1,53 +1,16 @@
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
 namespace DrawingApp
 {
     public partial class Paint : Form
     {
-        Graphics g;
-        Pen pen;
-
-        int x = -1;
-        int y = -1;
-        bool move = false;
 
         public Paint()
         {
             InitializeComponent();
-            g = panel1.CreateGraphics();
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            pen = new Pen(Color.Black, 5);
-            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-            PictureBox p = (PictureBox)sender;
-            pen.Color = p.BackColor;
-        }
 
-        private void panel1_MouseDown(object sender, MouseEventArgs e)
-        {
-            move = true;
-            x = e.X;
-            y = e.Y;
-            panel1.Cursor = Cursors.Cross;
-        }
-
-        private void panel1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (move && x != -1 && y != -1)
-            {
-                g.DrawLine(pen, new Point(x, y), e.Location);
-                x = e.X;
-                y = e.Y;
-            }
-        }
-
-        private void panel1_MouseUp(object sender, MouseEventArgs e)
-        {
-            move = false;
-            x = -1;
-            y = -1;
-            panel1.Cursor = Cursors.Default;
-        }
     }
 }
