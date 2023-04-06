3fb4f8f79f2b0126e4dca90ce1ac7b668673a73c
Hazim Okanovic <hazimokanovic@live.com>
2023-04-06 22:32:03 +0200

    Added the first buttons and one of the images

--- DrawingApp/DrawingApp.Designer.cs
+++ DrawingApp/DrawingApp.Designer.cs
@@ -31,7 +31,12 @@
             panel1 = new Panel();
             panel3 = new Panel();
             panel2 = new Panel();
+            pic = new PictureBox();
+            pic_color = new Button();
+            button1 = new Button();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pic).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -39,6 +44,7 @@
             panel1.BackColor = Color.White;
             panel1.Controls.Add(panel3);
             panel1.Controls.Add(panel2);
+            panel1.Controls.Add(pic);
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
@@ -57,11 +63,47 @@
             // panel2
             // 
             panel2.BackColor = Color.Black;
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(pic_color);
             panel2.Dock = DockStyle.Top;
             panel2.Location = new Point(0, 0);
             panel2.Name = "panel2";
             panel2.Size = new Size(800, 100);
             panel2.TabIndex = 0;
+            // 
+            // pic
+            // 
+            pic.Dock = DockStyle.Fill;
+            pic.Location = new Point(0, 0);
+            pic.Name = "pic";
+            pic.Size = new Size(800, 450);
+            pic.TabIndex = 2;
+            pic.TabStop = false;
+            // 
+            // pic_color
+            // 
+            pic_color.BackColor = Color.White;
+            pic_color.Location = new Point(166, 66);
+            pic_color.Name = "pic_color";
+            pic_color.Size = new Size(37, 34);
+            pic_color.TabIndex = 0;
+            pic_color.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
+            button1.FlatAppearance.MouseOverBackColor = Color.Black;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.White;
+            button1.Image = Properties.Resources.color;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(287, 44);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 53);
+            button1.TabIndex = 1;
+            button1.Text = "Color";
+            button1.TextAlign = ContentAlignment.BottomCenter;
+            button1.UseVisualStyleBackColor = true;
             // 
             // Paint
             // 
@@ -72,6 +114,8 @@
             Name = "Paint";
             Text = "Paint";
             panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pic).EndInit();
             ResumeLayout(false);
         }
 
@@ -80,5 +124,8 @@
         private Panel panel1;
         private Panel panel3;
         private Panel panel2;
+        private Button button1;
+        private Button pic_color;
+        private PictureBox pic;
     }
 }
