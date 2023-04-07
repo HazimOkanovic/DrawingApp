c328fee49326c714bd7475f55d3d80dda1ef34bd
Hazim Okanovic <hazimokanovic@live.com>
2023-04-07 12:04:26 +0200

    added function buttons on the top right corner

--- DrawingApp/DrawingApp.Designer.cs
+++ DrawingApp/DrawingApp.Designer.cs
@@ -31,11 +31,19 @@
             panel1 = new Panel();
             panel3 = new Panel();
             panel2 = new Panel();
-            pic = new PictureBox();
             pic_color = new Button();
-            button1 = new Button();
+            panel4 = new Panel();
+            btn_line = new Button();
+            btn_color = new Button();
+            btn_rect = new Button();
+            btn_fill = new Button();
+            btn_ellipse = new Button();
+            btn_pencil = new Button();
+            btn_eraser = new Button();
+            pic = new PictureBox();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
+            panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pic).BeginInit();
             SuspendLayout();
             // 
@@ -53,7 +61,7 @@
             // 
             // panel3
             // 
-            panel3.BackColor = Color.Black;
+            panel3.BackColor = Color.DimGray;
             panel3.Dock = DockStyle.Bottom;
             panel3.Location = new Point(0, 397);
             panel3.Name = "panel3";
@@ -62,23 +70,14 @@
             // 
             // panel2
             // 
-            panel2.BackColor = Color.Black;
-            panel2.Controls.Add(button1);
+            panel2.BackColor = Color.Gray;
             panel2.Controls.Add(pic_color);
+            panel2.Controls.Add(panel4);
             panel2.Dock = DockStyle.Top;
             panel2.Location = new Point(0, 0);
             panel2.Name = "panel2";
             panel2.Size = new Size(800, 100);
             panel2.TabIndex = 0;
-            // 
-            // pic
-            // 
-            pic.Dock = DockStyle.Fill;
-            pic.Location = new Point(0, 0);
-            pic.Name = "pic";
-            pic.Size = new Size(800, 450);
-            pic.TabIndex = 2;
-            pic.TabStop = false;
             // 
             // pic_color
             // 
@@ -89,21 +88,141 @@
             pic_color.TabIndex = 0;
             pic_color.UseVisualStyleBackColor = false;
             // 
-            // button1
+            // panel4
             // 
-            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
-            button1.FlatAppearance.MouseOverBackColor = Color.Black;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.ForeColor = Color.White;
-            button1.Image = Properties.Resources.color;
-            button1.ImageAlign = ContentAlignment.MiddleLeft;
-            button1.Location = new Point(287, 44);
-            button1.Name = "button1";
-            button1.Size = new Size(50, 53);
-            button1.TabIndex = 1;
-            button1.Text = "Color";
-            button1.TextAlign = ContentAlignment.BottomCenter;
-            button1.UseVisualStyleBackColor = true;
+            panel4.BackColor = Color.Black;
+            panel4.Controls.Add(btn_line);
+            panel4.Controls.Add(btn_color);
+            panel4.Controls.Add(btn_rect);
+            panel4.Controls.Add(btn_fill);
+            panel4.Controls.Add(btn_ellipse);
+            panel4.Controls.Add(btn_pencil);
+            panel4.Controls.Add(btn_eraser);
+            panel4.Location = new Point(263, 12);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(537, 78);
+            panel4.TabIndex = 8;
+            // 
+            // btn_line
+            // 
+            btn_line.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
+            btn_line.FlatAppearance.MouseOverBackColor = Color.Chocolate;
+            btn_line.FlatStyle = FlatStyle.Flat;
+            btn_line.ForeColor = Color.White;
+            btn_line.Image = Properties.Resources.line;
+            btn_line.ImageAlign = ContentAlignment.TopCenter;
+            btn_line.Location = new Point(442, 9);
+            btn_line.Name = "btn_line";
+            btn_line.Size = new Size(69, 60);
+            btn_line.TabIndex = 7;
+            btn_line.Text = "Line";
+            btn_line.TextAlign = ContentAlignment.BottomCenter;
+            btn_line.UseVisualStyleBackColor = true;
+            // 
+            // btn_color
+            // 
+            btn_color.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
+            btn_color.FlatAppearance.MouseOverBackColor = Color.Chocolate;
+            btn_color.FlatStyle = FlatStyle.Flat;
+            btn_color.ForeColor = Color.White;
+            btn_color.Image = Properties.Resources.color;
+            btn_color.ImageAlign = ContentAlignment.TopCenter;
+            btn_color.Location = new Point(21, 10);
+            btn_color.Name = "btn_color";
+            btn_color.Size = new Size(61, 60);
+            btn_color.TabIndex = 1;
+            btn_color.Text = "Color";
+            btn_color.TextAlign = ContentAlignment.BottomCenter;
+            btn_color.UseVisualStyleBackColor = true;
+            // 
+            // btn_rect
+            // 
+            btn_rect.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
+            btn_rect.FlatAppearance.MouseOverBackColor = Color.Chocolate;
+            btn_rect.FlatStyle = FlatStyle.Flat;
+            btn_rect.ForeColor = Color.White;
+            btn_rect.Image = Properties.Resources.rectangle;
+            btn_rect.ImageAlign = ContentAlignment.TopCenter;
+            btn_rect.Location = new Point(367, 9);
+            btn_rect.Name = "btn_rect";
+            btn_rect.Size = new Size(69, 60);
+            btn_rect.TabIndex = 6;
+            btn_rect.Text = "Rectangle";
+            btn_rect.TextAlign = ContentAlignment.BottomCenter;
+            btn_rect.UseVisualStyleBackColor = true;
+            // 
+            // btn_fill
+            // 
+            btn_fill.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
+            btn_fill.FlatAppearance.MouseOverBackColor = Color.Chocolate;
+            btn_fill.FlatStyle = FlatStyle.Flat;
+            btn_fill.ForeColor = Color.White;
+            btn_fill.Image = Properties.Resources.bucket;
+            btn_fill.ImageAlign = ContentAlignment.TopCenter;
+            btn_fill.Location = new Point(88, 9);
+            btn_fill.Name = "btn_fill";
+            btn_fill.Size = new Size(63, 60);
+            btn_fill.TabIndex = 2;
+            btn_fill.Text = "Fill";
+            btn_fill.TextAlign = ContentAlignment.BottomCenter;
+            btn_fill.UseVisualStyleBackColor = true;
+            // 
+            // btn_ellipse
+            // 
+            btn_ellipse.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
+            btn_ellipse.FlatAppearance.MouseOverBackColor = Color.Chocolate;
+            btn_ellipse.FlatStyle = FlatStyle.Flat;
+            btn_ellipse.ForeColor = Color.White;
+            btn_ellipse.Image = Properties.Resources.circle;
+            btn_ellipse.ImageAlign = ContentAlignment.TopCenter;
+            btn_ellipse.Location = new Point(297, 9);
+            btn_ellipse.Name = "btn_ellipse";
+            btn_ellipse.Size = new Size(64, 60);
+            btn_ellipse.TabIndex = 5;
+            btn_ellipse.Text = "Ellipse";
+            btn_ellipse.TextAlign = ContentAlignment.BottomCenter;
+            btn_ellipse.UseVisualStyleBackColor = true;
+            // 
+            // btn_pencil
+            // 
+            btn_pencil.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
+            btn_pencil.FlatAppearance.MouseOverBackColor = Color.Chocolate;
+            btn_pencil.FlatStyle = FlatStyle.Flat;
+            btn_pencil.ForeColor = Color.White;
+            btn_pencil.Image = Properties.Resources.pencil;
+            btn_pencil.ImageAlign = ContentAlignment.TopCenter;
+            btn_pencil.Location = new Point(157, 9);
+            btn_pencil.Name = "btn_pencil";
+            btn_pencil.Size = new Size(64, 60);
+            btn_pencil.TabIndex = 3;
+            btn_pencil.Text = "Pencil";
+            btn_pencil.TextAlign = ContentAlignment.BottomCenter;
+            btn_pencil.UseVisualStyleBackColor = true;
+            // 
+            // btn_eraser
+            // 
+            btn_eraser.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
+            btn_eraser.FlatAppearance.MouseOverBackColor = Color.Chocolate;
+            btn_eraser.FlatStyle = FlatStyle.Flat;
+            btn_eraser.ForeColor = Color.White;
+            btn_eraser.Image = Properties.Resources.eraser;
+            btn_eraser.ImageAlign = ContentAlignment.TopCenter;
+            btn_eraser.Location = new Point(227, 9);
+            btn_eraser.Name = "btn_eraser";
+            btn_eraser.Size = new Size(64, 60);
+            btn_eraser.TabIndex = 4;
+            btn_eraser.Text = "Eraser";
+            btn_eraser.TextAlign = ContentAlignment.BottomCenter;
+            btn_eraser.UseVisualStyleBackColor = true;
+            // 
+            // pic
+            // 
+            pic.Dock = DockStyle.Fill;
+            pic.Location = new Point(0, 0);
+            pic.Name = "pic";
+            pic.Size = new Size(800, 450);
+            pic.TabIndex = 2;
+            pic.TabStop = false;
             // 
             // Paint
             // 
@@ -115,6 +234,7 @@
             Text = "Paint";
             panel1.ResumeLayout(false);
             panel2.ResumeLayout(false);
+            panel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pic).EndInit();
             ResumeLayout(false);
         }
@@ -124,8 +244,15 @@
         private Panel panel1;
         private Panel panel3;
         private Panel panel2;
-        private Button button1;
+        private Button btn_color;
         private Button pic_color;
         private PictureBox pic;
+        private Button btn_line;
+        private Button btn_rect;
+        private Button btn_ellipse;
+        private Button btn_eraser;
+        private Button btn_pencil;
+        private Button btn_fill;
+        private Panel panel4;
     }
 }
--- DrawingApp/DrawingApp.cs
+++ DrawingApp/DrawingApp.cs
@@ -10,7 +10,5 @@ namespace DrawingApp
         {
             InitializeComponent();
         }
-
-
     }
 }
