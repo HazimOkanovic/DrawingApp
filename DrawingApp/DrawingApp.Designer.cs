169ac53131b3c0343b123554495469825fa2eb85
Hazim Okanovic <hazimokanovic@live.com>
2023-04-10 12:21:41 +0200

    Finished with app development, everything is working great

--- DrawingApp/DrawingApp.Designer.cs
+++ DrawingApp/DrawingApp.Designer.cs
@@ -34,6 +34,8 @@
             color_picker = new PictureBox();
             pic_color = new Button();
             panel4 = new Panel();
+            btn_save = new Button();
+            btn_clear = new Button();
             btn_line = new Button();
             btn_color = new Button();
             btn_rect = new Button();
@@ -84,18 +86,20 @@
             // 
             // color_picker
             // 
+            color_picker.Cursor = Cursors.Hand;
             color_picker.Image = Properties.Resources.color_palette;
-            color_picker.Location = new Point(3, 3);
+            color_picker.Location = new Point(3, 0);
             color_picker.Name = "color_picker";
-            color_picker.Size = new Size(281, 95);
+            color_picker.Size = new Size(291, 98);
             color_picker.SizeMode = PictureBoxSizeMode.StretchImage;
             color_picker.TabIndex = 9;
             color_picker.TabStop = false;
+            color_picker.MouseClick += color_picker_MouseClick;
             // 
             // pic_color
             // 
             pic_color.BackColor = Color.White;
-            pic_color.Location = new Point(290, 44);
+            pic_color.Location = new Point(300, 36);
             pic_color.Name = "pic_color";
             pic_color.Size = new Size(24, 27);
             pic_color.TabIndex = 0;
@@ -104,6 +108,8 @@
             // panel4
             // 
             panel4.BackColor = Color.Black;
+            panel4.Controls.Add(btn_save);
+            panel4.Controls.Add(btn_clear);
             panel4.Controls.Add(btn_line);
             panel4.Controls.Add(btn_color);
             panel4.Controls.Add(btn_rect);
@@ -111,13 +117,46 @@
             panel4.Controls.Add(btn_ellipse);
             panel4.Controls.Add(btn_pencil);
             panel4.Controls.Add(btn_eraser);
-            panel4.Location = new Point(320, 21);
+            panel4.Location = new Point(330, 12);
             panel4.Name = "panel4";
-            panel4.Size = new Size(480, 69);
+            panel4.Size = new Size(537, 69);
             panel4.TabIndex = 8;
+            // 
+            // btn_save
+            // 
+            btn_save.Cursor = Cursors.Hand;
+            btn_save.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
+            btn_save.FlatAppearance.MouseOverBackColor = Color.Chocolate;
+            btn_save.FlatStyle = FlatStyle.Flat;
+            btn_save.ForeColor = Color.White;
+            btn_save.ImageAlign = ContentAlignment.TopCenter;
+            btn_save.Location = new Point(478, 4);
+            btn_save.Name = "btn_save";
+            btn_save.Size = new Size(56, 25);
+            btn_save.TabIndex = 10;
+            btn_save.Text = "Save";
+            btn_save.UseVisualStyleBackColor = true;
+            btn_save.Click += btn_save_Click;
+            // 
+            // btn_clear
+            // 
+            btn_clear.Cursor = Cursors.Hand;
+            btn_clear.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
+            btn_clear.FlatAppearance.MouseOverBackColor = Color.Chocolate;
+            btn_clear.FlatStyle = FlatStyle.Flat;
+            btn_clear.ForeColor = Color.White;
+            btn_clear.ImageAlign = ContentAlignment.TopCenter;
+            btn_clear.Location = new Point(478, 35);
+            btn_clear.Name = "btn_clear";
+            btn_clear.Size = new Size(56, 27);
+            btn_clear.TabIndex = 8;
+            btn_clear.Text = "Clear";
+            btn_clear.UseVisualStyleBackColor = true;
+            btn_clear.Click += btn_clear_Click;
             // 
             // btn_line
             // 
+            btn_line.Cursor = Cursors.Hand;
             btn_line.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
             btn_line.FlatAppearance.MouseOverBackColor = Color.Chocolate;
             btn_line.FlatStyle = FlatStyle.Flat;
@@ -131,9 +170,11 @@
             btn_line.Text = "Line";
             btn_line.TextAlign = ContentAlignment.BottomCenter;
             btn_line.UseVisualStyleBackColor = true;
+            btn_line.Click += btn_line_Click;
             // 
             // btn_color
             // 
+            btn_color.Cursor = Cursors.Hand;
             btn_color.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
             btn_color.FlatAppearance.MouseOverBackColor = Color.Chocolate;
             btn_color.FlatStyle = FlatStyle.Flat;
@@ -147,9 +188,11 @@
             btn_color.Text = "Color";
             btn_color.TextAlign = ContentAlignment.BottomCenter;
             btn_color.UseVisualStyleBackColor = true;
+            btn_color.Click += btn_color_Click;
             // 
             // btn_rect
             // 
+            btn_rect.Cursor = Cursors.Hand;
             btn_rect.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
             btn_rect.FlatAppearance.MouseOverBackColor = Color.Chocolate;
             btn_rect.FlatStyle = FlatStyle.Flat;
@@ -163,9 +206,11 @@
             btn_rect.Text = "Rectangle";
             btn_rect.TextAlign = ContentAlignment.BottomCenter;
             btn_rect.UseVisualStyleBackColor = true;
+            btn_rect.Click += btn_rect_Click;
             // 
             // btn_fill
             // 
+            btn_fill.Cursor = Cursors.Hand;
             btn_fill.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
             btn_fill.FlatAppearance.MouseOverBackColor = Color.Chocolate;
             btn_fill.FlatStyle = FlatStyle.Flat;
@@ -179,9 +224,11 @@
             btn_fill.Text = "Fill";
             btn_fill.TextAlign = ContentAlignment.BottomCenter;
             btn_fill.UseVisualStyleBackColor = true;
+            btn_fill.Click += btn_fill_Click;
             // 
             // btn_ellipse
             // 
+            btn_ellipse.Cursor = Cursors.Hand;
             btn_ellipse.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
             btn_ellipse.FlatAppearance.MouseOverBackColor = Color.Chocolate;
             btn_ellipse.FlatStyle = FlatStyle.Flat;
@@ -195,9 +242,11 @@
             btn_ellipse.Text = "Ellipse";
             btn_ellipse.TextAlign = ContentAlignment.BottomCenter;
             btn_ellipse.UseVisualStyleBackColor = true;
+            btn_ellipse.Click += btn_ellipse_Click;
             // 
             // btn_pencil
             // 
+            btn_pencil.Cursor = Cursors.Hand;
             btn_pencil.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
             btn_pencil.FlatAppearance.MouseOverBackColor = Color.Chocolate;
             btn_pencil.FlatStyle = FlatStyle.Flat;
@@ -215,6 +264,7 @@
             // 
             // btn_eraser
             // 
+            btn_eraser.Cursor = Cursors.Hand;
             btn_eraser.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 0, 0);
             btn_eraser.FlatAppearance.MouseOverBackColor = Color.Chocolate;
             btn_eraser.FlatStyle = FlatStyle.Flat;
@@ -228,6 +278,7 @@
             btn_eraser.Text = "Eraser";
             btn_eraser.TextAlign = ContentAlignment.BottomCenter;
             btn_eraser.UseVisualStyleBackColor = true;
+            btn_eraser.Click += btn_eraser_Click;
             // 
             // pic
             // 
@@ -237,6 +288,8 @@
             pic.Size = new Size(800, 450);
             pic.TabIndex = 2;
             pic.TabStop = false;
+            pic.Paint += pic_Paint;
+            pic.MouseClick += pic_MouseClick;
             pic.MouseDown += pic_MouseDown;
             pic.MouseMove += pic_MouseMove;
             pic.MouseUp += pic_MouseUp;
@@ -273,5 +326,7 @@
         private Button btn_fill;
         private Panel panel4;
         private PictureBox color_picker;
+        private Button btn_clear;
+        private Button btn_save;
     }
 }
